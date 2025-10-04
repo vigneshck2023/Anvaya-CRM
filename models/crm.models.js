c991e77e0a84f0c1c16d78a1a5cc9aab66e04184
Vignesh C K <vigneshck2003.code@gmail.com>
2025-10-04 21:24:35 +0530

    modifying the schema for salesAgent

--- models/crm.models.js
+++ models/crm.models.js
@@ -1,69 +1,75 @@
 const mongoose = require("mongoose");
 
-// salesAgent Schema
-const salesAgentSchema = new mongoose.Schema({
+// SalesAgent Schema
+const salesAgentSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true
     },
     phone: {
-        type: String
+      type: String
     },
     role: {
-        type: String,
+      type: String
     }
-},
-{timestamps: true});
+  },
+  { timestamps: true }
+);
 
 const SalesAgent = mongoose.model("SalesAgent", salesAgentSchema);
 
 // Lead Schema
-
-const leadSchema = new mongoose.Schema({
+const leadSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     source: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-    salesAgent: {
+    salesAgent: [
+      {
         type: mongoose.Schema.Types.ObjectId,
         ref: "SalesAgent",
         required: true
-    },
+      }
+    ],
 
     status: {
-        type: String,
-        enum: ["New", "Contracted", "Qualified"],
-        default: "New",
-        required: true
+      type: String,
+      enum: ["New", "Contracted", "Qualified"],
+      default: "New",
+      required: true
     },
-    tags: [{type: String}],
+    tags: [{ type: String }],
     timeToClose: {
-        type: Number
+      type: Number
     },
     priority: {
-        type: String,
-        enum: ["High", "Medium", "Low"],
-        default: "Medium",
-        required: true
+      type: String,
+      enum: ["High", "Medium", "Low"],
+      default: "Medium",
+      required: true
     },
     comments: [
-    {
-      author: String,
-      text: String,
-      date: { type: Date, default: Date.now }
-    }
-  ]
-}, {timestamps: true});
+      {
+        author: String,
+        text: String,
+        date: { type: Date, default: Date.now }
+      }
+    ]
+  },
+  { timestamps: true }
+);
 
 const Lead = mongoose.model("Lead", leadSchema);
 
-module.exports = {SalesAgent, Lead};
+module.exports = { SalesAgent, Lead };
